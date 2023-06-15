004f4f81056f7c124cf5bfe108dec6f7b7072e6f
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-06-15 16:32:29 -0400

    test graphql client

--- drop-mobile/.eslintrc.cjs
+++ drop-mobile/.eslintrc.cjs
@@ -1,14 +1,32 @@
 module.exports = {
+	root: true,
 	env: { browser: true, es2020: true },
 	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:react-hooks/recommended",
 	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-	plugins: ["react-refresh"],
 	rules: {
-		"react-refresh/only-export-components": "warn",
+		"react/function-component-definition": 0,
+		"react/react-in-jsx-scope": 0,
+		"import/prefer-default-export": 0,
+		"react-hooks/exhaustive-deps": 1,
+		"jsx-a11y/click-events-have-key-events": 0,
+		"jsx-a11y/no-static-element-interactions": 0,
+	},
+	parser: require.resolve("@typescript-eslint/parser"),
+	parserOptions: {
+		project: require.resolve("./tsconfig.json"),
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+		"import/resolver": {
+			alias: {
+				map: [["@", "./src"]],
+				extensions: [".ts", ".tsx"],
+			},
+		},
 	},
 };
--- drop-mobile/src/main.tsx
+++ drop-mobile/src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./utils/apollo";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
+	<ApolloProvider client={client}>
 		<App />
-	</React.StrictMode>
+	</ApolloProvider>
 );
