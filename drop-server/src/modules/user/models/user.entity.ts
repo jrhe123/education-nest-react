7a45b99f7fdccf5af2791944c7cc6352b06ba27d
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-06-14 21:53:46 -0400

    add mysql docker

--- drop-server/src/modules/user/models/user.entity.ts
+++ drop-server/src/modules/user/models/user.entity.ts
@@ -1,7 +1,35 @@
+import { Column, Entity } from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
+@Entity('user')
 export class User {
+  @Column({
+    comment: 'name',
+    default: '',
+  })
+  @IsNotEmpty()
   name: string;
+
+  @Column({
+    comment: 'description',
+    default: '',
+  })
   desc: string;
+
+  @Column({
+    comment: 'phone number',
+    nullable: true,
+  })
   tel: string;
+
+  @Column({
+    comment: 'password',
+    nullable: true,
+  })
   password: string;
+
+  @Column({
+    comment: 'account info',
+    nullable: true,
+  })
   account: string;
 }
